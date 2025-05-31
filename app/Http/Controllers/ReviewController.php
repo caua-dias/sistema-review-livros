13ae3858a914dcd1640d9cfcd50ef1bb666bc71d
caua.dias <caua.dias.cd0@gmail.com>
2025-05-30 22:30:11 -0300

    wip: validando os dados para review

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\ReviewService;
+use App\Http\Requests\ReviewStoreRequest;
+use App\Http\Requests\ReviewUpdateRequest;
+use App\Http\Resources\ReviewResource;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class ReviewController extends Controller
+{
+        private ReviewService $reviewService;
+    public function __construct(ReviewService $reviewService) {
+        $this->reviewService = $reviewService;
+    }
+
+    public function get() {
+        $reviews = $this->reviewService->get();
+        return ReviewResource::collection($reviews);
+    }
+
+    public function details(int $id){
+        try{
+            $reviews = $this->reviewService->details($id);
+        }catch(ModelNotFoundException $e){
+            return response()->json(['error'=>'Review not found'],404);
+        }
+        return new ReviewResource($reviews);
+    }
+
+    public function store(ReviewStoreRequest $request){
+        $data = $request->validated();
+        $review = $this->reviewService->store($data);
+        return new ReviewResource($review);
+    }
+
+    public function update(int $id, ReviewUpdateRequest $request)
+    {
+        $data = $request->validated();
+        try{
+            $review = $this->reviewService->update($id,$data);
+        }catch(ModelNotFoundException $e){
+            return response()->json(['error'=>'Review not found'],404);
+        }
+        return new ReviewResource($review);
+
+    }
+
+    public function delete(int $id)
+    {
+        try{
+            $review = $this->reviewService->delete($id);
+        }catch(ModelNotFoundException $e){
+            return response()->json(['error'=>'Review not found'],404);
+        }
+        return new ReviewResource($review);
+    }
+}
