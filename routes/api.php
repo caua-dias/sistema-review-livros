e86cd7bca03a98d78767dcbb886df02fc4f21657
Miguel Luz <106433901+Migleu@users.noreply.github.com>
2025-05-30 22:04:30 -0300

    feat: passando as rotas do readerController

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,10 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/readers',[ReaderController::class,'get']);
+Route::controller(ReaderController::class)->group( function() {
+    Route::get('/readers','get');
+    Route::get('/readers/{id}', 'details');
+    Route::post('/readers','store');
+    Route::patch('/readers/{id}', 'update');
+    Route::delete('/readers/{id}','delete');
+});
