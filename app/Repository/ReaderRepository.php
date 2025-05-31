7ae97966bbdc0fd976a3356dd8c66e3aa2db3297
Miguel Luz <106433901+Migleu@users.noreply.github.com>
2025-05-30 22:03:56 -0300

    feat: acesso para o banco de dados

--- app/Repository/ReaderRepository.php
+++ app/Repository/ReaderRepository.php
@@ -5,8 +5,31 @@ use App\Models\Reader;
 
 class ReaderRepository
 {
- public function get()
- {
-    return Reader::all();
- }
+public function get()
+   {
+      return Reader::all();
+   }
+
+   public function details(int $id)
+    {
+        return Reader::find($id);
+    }
+public function store(array $data)
+    {
+        return Reader::create($data);
+    }
+
+public function update(int $id, array $data)
+    {
+        $reader = $this->details($id);
+        $reader->update($data);
+        return $reader;
+    }
+public function delete(int $id)
+    {
+        $reader = $this->details($id);
+        $reader->delete();
+        return $reader;
+    }
+
 }
