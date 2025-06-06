e9283728364aceb5239dce73c9a3e8ee6e537d19
caua-dias <caua.dias.cd0@gmail.com>
2025-06-06 15:41:40 -0300

    feat: relacionamento hasMany e belongsTo

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -7,5 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     protected $table = 'books';
-    protected $fillable = ['title','synopsis' ];
+    protected $fillable = ['title','synopsis','author_id, genre_id' ];
+
+    public function authors(): BelongsTo
+    {
+        return $this->BelongsTo(Author::class);
+    }
+
+    public function genres(): BelongsTo
+    {
+        return $this->BelongsTo(Genre::class);
+    }
+
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(Review::class);
+    }
 }
