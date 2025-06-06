b42fdc8a5d4eb5b5784668d2e9c4f2d0b0032582
caua-dias <caua.dias.cd0@gmail.com>
2025-06-06 15:44:21 -0300

    feat: migration reader and book id's na table de reviews

---
+++ database/migrations/2025_06_06_180952_alter_reviews_table_add_reader_and_book_id.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->unsignedBigInteger('reader_id')->after('text');
+            $table->unsignedBigInteger('book_id')->after('reader_id');
+            
+            $table->foreign('reader_id')->references('id')->on('readers')->onDelete('cascade');
+            $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
