3b508aecb3f264205c8a0dd93accac111fba26b4
caua-dias <caua.dias.cd0@gmail.com>
2025-06-06 15:41:50 -0300

    feat: relacionamento belongsTo

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -7,5 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     protected $table = 'reviews';
-    protected $fillable = ['grade','text' ];
+    protected $fillable = ['grade','text', 'reader_id', 'book_id' ];
+
+    public function reader(): BelongsTo
+    {
+        return $this->belongsTo(Reader::class);
+    }
+
+    public function book(): BelongsTo
+    {
+        return $this->belongsTo(Book::class);
+    }
 }
